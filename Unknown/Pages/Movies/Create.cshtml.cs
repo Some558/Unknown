aaec589d8bb347dd7b7236365c7e489211f6b024
Some558 <1641043@gmail.com>
2025-04-12 06:21:50 +0900

    CRUD処理追加

---
+++ Unknown/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+using Unknown.Data;
+
+namespace Unknown.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Unknown.Data.UnknownContext _context;
+
+        public CreateModel(Unknown.Data.UnknownContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Unknown/Program.cs
+++ Unknown/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Unknown.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<UnknownContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UnknownContext") ?? throw new InvalidOperationException("Connection string 'UnknownContext' not found.")));
 
 var app = builder.Build();
 
